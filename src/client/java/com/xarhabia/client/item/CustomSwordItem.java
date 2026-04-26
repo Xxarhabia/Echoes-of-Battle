3975d343a5f49e987fad339c019a05b3ef6b03b5
Mario Sarabia <msarabiacarrascal@gmail.com>
2026-04-26 02:37:15 -0500

    ➕ Agregamos bonus damage a la espada e identificamos el daño base, bonus y daño total

--- src/client/java/com/xarhabia/client/item/CustomSwordItem.java
+++ src/client/java/com/xarhabia/client/item/CustomSwordItem.java
@@ -5,6 +5,8 @@ import com.xarhabia.stats.PlayerStats;
 import com.xarhabia.stats.PlayerStatsManager;
 import net.fabricmc.fabric.api.client.item.v1.ItemTooltipCallback;
 import net.minecraft.client.MinecraftClient;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 
@@ -21,8 +23,23 @@ public class CustomSwordItem {
 
             double bonus = stats.getAgility() * 0.4;
 
-            tooltip.add(Text.literal(" "));
-            tooltip.add(Text.literal("Bonus por agilidad: +" + (int) bonus)
+            double baseDamage = 0;
+
+            var modifiers = stack.getAttributeModifiers(net.minecraft.entity.EquipmentSlot.MAINHAND)
+                    .get(EntityAttributes.GENERIC_ATTACK_DAMAGE);
+
+            for (EntityAttributeModifier mod: modifiers) {
+                baseDamage += mod.getValue();
+            }
+
+            double total = baseDamage + bonus;
+
+            tooltip.removeIf(text -> text.getString().contains("Daño por golpe"));
+            tooltip.add(Text.literal("Daño total: " + (int) total)
+                    .formatted(Formatting.RED));
+            tooltip.add(Text.literal("Base: " + (int) baseDamage)
+                    .formatted(Formatting.GRAY));
+            tooltip.add(Text.literal("Bonus: " + (int) bonus)
                     .formatted(Formatting.GREEN));
         });
     }
